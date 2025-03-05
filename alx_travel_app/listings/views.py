84053afc3da81a6d3d1ee8e32e5a72244e22cb14
DINAMOW <meemoo102039@gmail.com>
2025-03-06 01:52:35 +0200

    Adds API endpoints for listings and bookings
    Implements API endpoints for managing listings and bookings,
    providing CRUD operations through ViewSets.
    
    These endpoints allow users to interact with listing and booking
    data in a structured way.

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ListingSerializer, BookingSerializer
+from .models import Listing, Booking
 
-# Create your views here.
+
+class ListingViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for viewing and editing listings.
+    Provides CRUD operations for Listing model.
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for viewing and editing bookings.
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class InitiatePaymentView(viewsets.ModelViewSet):
+    """
+    View for initiating a payment.
+    """
